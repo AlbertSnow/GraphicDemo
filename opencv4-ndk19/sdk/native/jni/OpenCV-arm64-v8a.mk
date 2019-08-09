b579631dfd704a03e33794f55c410d388e51fc23
ZhaoJiaLiang <zhaojialiang@sogou-inc.com>
2019-08-09 20:45:28 +0800

    1. add opencv native library

---
+++ opencv4-ndk19/sdk/native/jni/OpenCV-arm64-v8a.mk
@@ -0,0 +1,2 @@
+OPENCV_3RDPARTY_COMPONENTS:=cpufeatures libprotobuf libjpeg-turbo libwebp libpng libtiff libjasper IlmImf quirc tegra_hal
+OPENCV_EXTRA_COMPONENTS:=/Users/apple/Library/Android/sdk/ndk-bundle/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/lib/aarch64-linux-android/libz.a freetype harfbuzz dl m log
