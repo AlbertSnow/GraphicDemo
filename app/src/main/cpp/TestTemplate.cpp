05d0dad753c3eae9199711ac24cae99b7dc84240
赵佳亮 <zhaojialiang@sogou-inc.com>
2019-10-23 18:34:34 +0800

    Complete template method

--- app/src/main/cpp/TestTemplate.cpp
+++ app/src/main/cpp/TestTemplate.cpp
@@ -45,7 +45,21 @@ T testNoTypeTemplate(T val) {
 //};
 
 template<class E>
+class MyPair;
+
+
+class MyPairFriend {
+
+public:
+    template<class E>
+    void testFriend(MyPair<E> myPair);
+};
+
+
+template<class E>
 class MyPair {
+    friend class MyPairFriend;
+private:
     E value[2];
 
 public:
@@ -57,10 +71,39 @@ public:
 
     E getMax();
 
+    void friendMethod(const MyPair&);
+
+    template<class H>
+    friend void noMemberFriendMethod(const MyPair<H>&);
+
     ~MyPair(){
         cout << "I'm destructor \n";
     }
 };
+
+template<class E>
+void MyPair<E>::friendMethod(const MyPair<E>& param) {
+    cout << "friend method: " << this->value[1];
+    cout << "param friend method: " << param.value[1];
+}
+
+template<class E>
+void noMemberFriendMethod(const MyPair<E>& param) {
+    cout << "no member param friend method: " << param.value[1];
+}
+
+
+template<class E>
+void MyPairFriend::testFriend(MyPair<E> myPair) {
+    cout << "My friend Pair: " << myPair.value[0];
+}
+
+void testFriend() {
+    MyPair<int> myPair (1, 2);
+    MyPairFriend myPairFriend;
+
+    myPairFriend.testFriend(myPair);
+}
 
 
 
@@ -71,7 +114,7 @@ E MyPair<E>::getMax() {
     return retvl;
 }
 
-int main() {
+void testTemplate() {
     int i = 5, j = 6, k;
     double f = 2.0, g = 0.5, h;
 
@@ -97,8 +140,18 @@ int main() {
     MyPair<int> instance2 (4, 12);
     cout << "max value2: " << instance2.getMax() << "\n";
 
-    delete instance; // dynamic memory need call delete key word
+    instance->friendMethod(instance1);
+    noMemberFriendMethod(instance1);
 
+    delete instance; // dynamic memory need call delete key word
+}
+
+
+
+int main() {
+
+    testTemplate();
+    testFriend();
     return 0;
 }
 
