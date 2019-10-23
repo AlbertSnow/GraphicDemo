57c899859b35413ebb4bf087879f23f437e84076
赵佳亮 <zhaojialiang@sogou-inc.com>
2019-10-23 20:38:17 +0800

    1.Add inherit test  2. change the cmakelist for IDE

---
+++ app/src/main/cpp/TestInheritance.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Apple on 2019-10-23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+
+protected:
+    int age;
+
+public:
+    int publicAge;
+
+    Animal(){
+        cout  << "animal construct \n";
+    }
+};
+
+// protected将成为pubicAge的access level
+class Human : protected Animal {
+
+public:
+    Human() {
+        cout << "Im human construct, age: " << age;
+    }
+
+};
+
+
+int main() {
+    Human human;
+
+    cout << "create human over \n";
+
+    //protected修饰Animal，导致所有继承自Animal的member，最多是protected属性
+//    cout << "public age: " << human.publicAge << "\n";
+
+    return 0;
+}
+
+
