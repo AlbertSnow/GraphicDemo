0fed9565d87b5f464165b3ae33e84b820a915606
赵佳亮 <zhaojialiang@sogou-inc.com>
2020-08-25 10:07:48 +0800

    1. use package name mapping cpp file name 2.epoll add ctl to regular file will failure

--- app/src/main/cpp/com_albertsnow_graphicdemo_looper_MyLooper.cpp
+++ app/src/main/cpp/com_albertsnow_graphicdemo_looper_MyLooper.cpp
@@ -3,21 +3,20 @@
 //
 #include "com_albertsnow_graphicdemo_looper_MyLooper.h"
 
-#include <jni.h>
 #include <android/log.h>
 #include <jni.h>
-#include <jni.h>
+#include "loop/MyLooper.h"
 
 #define TAG "TestException"
 #define LOGE(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__)
 
 
 extern "C" JNIEXPORT void JNICALL Java_com_albertsnow_graphicdemo_looper_MyLooper_init(JNIEnv
-* env, jobject thiz
-) {
-
+* env, jobject thiz) {
+    init(env, thiz);
 }
 
 extern "C" JNIEXPORT void JNICALL Java_com_albertsnow_graphicdemo_looper_MyLooper_wait(JNIEnv
 * env, jobject thiz, jint timeout) {
+    loopNext(env, thiz, timeout);
 }
