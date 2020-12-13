2656522eeba88398afc3a8cf69f416dc043b52b6
AlbertSnow <Albert.Snow.Email@gmail.com>
2020-12-13 17:55:53 +0800

    Add hough line detect

--- opencvlibrary/src/main/cpp/com/albert/opencvlibrary/PictureUtils.cpp
+++ opencvlibrary/src/main/cpp/com/albert/opencvlibrary/PictureUtils.cpp
@@ -9,6 +9,7 @@
 #include "DebugUtils.h"
 
 using namespace cv;
+using namespace std;
 
 int main(void)
 {
@@ -26,14 +27,39 @@ int main(void)
     }
 
 
-    Mat img = imread(picturePath);
-    Mat dest(img.rows, img.cols, CV_8UC1);
-    cvtColor(img, dest, COLOR_BGR2GRAY);
+    Mat dst, cdst, cdstP;
 
-    imwrite(writePath, dest);
+    Mat src = imread(picturePath,IMREAD_GRAYSCALE);
 
+    if (src.empty()) {
+        logStr("Load picture empty");
+        return -1;
+    }
 
+    Canny(src, dst, 50, 200, 3);
+    cvtColor(dst, cdst, COLOR_GRAY2BGR);
+    cdstP = cdst.clone();
 
+    vector<Vec2f> lines;
+    HoughLines(dst, lines, 1, CV_PI / 180, 150, 0, 0);
+
+    for (size_t i =0; i < lines.size(); i++) {
+        float rho = lines[i][0], theta = lines[i][1];
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+        line( cdst, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
+    }
+
+    if (cdst.empty()) {
+        printf("--__- hough line is empty");
+    } else {
+        imwrite(writePath, cdst);
+    }
     printf("-------- Every body see that --------");
 
     return 0;
