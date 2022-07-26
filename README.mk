3861526c11339baf8ad76dbdaca904d2166b79ce
zhaojialiang02 <zhaojialiang02@meituan.com>
2022-07-26 10:59:32 +0800

    1. add README.mk file to help build phase

---
+++ README.mk
@@ -0,0 +1,9 @@
+## Build instruction
+1. Download NDK version from SDK Manager
+    - My test version is 20.0.5594570
+2. add your NDK directory to local.properties file
+
+```
+sdk.dir=/Users/youName/Library/Android/sdk
+ndk.dir=/Users/youName/Library/Android/sdk/ndk/20.0.5594570
+```
